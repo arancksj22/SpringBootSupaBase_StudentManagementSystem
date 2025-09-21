1abcc77fb70e3ec900408147b8623abf0ea40b8e
arancksj22 <arancksj@gmail.com>
2025-09-21 20:16:08 +0530

    service layer first half methods

--- src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/MainService.java
+++ src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/MainService.java
@@ -1,4 +1,26 @@
 package com.example.SpringBootSupaBase_StudentManagementSystem;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MainService {
+
+    MainRepository repo;
+
+    MainService(MainRepository repo){
+        this.repo = repo;
+    }
+
+    public List<StudentModel> viewAllStudents(){
+        return repo.findAll();
+    }
+
+    public StudentModel findById(int id){
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("No Student with Id"));
+    }
+
+
 }
