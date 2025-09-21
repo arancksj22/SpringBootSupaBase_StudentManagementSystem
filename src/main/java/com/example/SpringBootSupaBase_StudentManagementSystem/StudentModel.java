2567783e94418ab4c55e7be3f484544f50992290
arancksj22 <arancksj@gmail.com>
2025-09-21 20:02:25 +0530

    student model, with attributes and methods

--- src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/StudentModel.java
+++ src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/StudentModel.java
@@ -7,10 +7,69 @@ import jakarta.persistence.Id;
 public class StudentModel {
 
     @Id
-    private final int id;
+    private int id;
 
-    private final int roll;
-    private final int marks;
-    private final long sapId;
+    private int roll;
+    private char division;
+    private int marks;
+    private long sapId;
 
+    public StudentModel(int id, int roll, char division, int marks, long sapId) {
+        this.id = id;
+        this.roll = roll;
+        this.division = division;
+        this.marks = marks;
+        this.sapId = sapId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public char getDivision() {
+        return division;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public long getSapId() {
+        return sapId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setDivision(char division) {
+        this.division = division;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public void setSapId(long sapId) {
+        this.sapId = sapId;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "id=" + id +
+                ", roll=" + roll +
+                ", division=" + division +
+                ", marks=" + marks +
+                ", sapId=" + sapId +
+                '}';
+    }
 }
