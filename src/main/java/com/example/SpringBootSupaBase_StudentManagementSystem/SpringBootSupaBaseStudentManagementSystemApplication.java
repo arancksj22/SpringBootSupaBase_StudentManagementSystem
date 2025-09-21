dc942e73e3b9f34e23a0f2def3217ec27999371b
arancksj22 <arancksj@gmail.com>
2025-09-21 19:52:52 +0530

    springboot skeleton

---
+++ src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/SpringBootSupaBaseStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSupaBase_StudentManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSupaBaseStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSupaBaseStudentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootSupaBase_StudentManagementSystem/SpringBootSupaBaseStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSupaBase_StudentManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSupaBaseStudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
