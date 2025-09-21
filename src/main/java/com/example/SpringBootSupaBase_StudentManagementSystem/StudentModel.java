061b5c2528f621619564c93a191e6534ccd2d2b1
arancksj22 <arancksj@gmail.com>
2025-09-21 19:56:42 +0530

    added attributes to model

--- src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/StudentModel.java
+++ src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/StudentModel.java
@@ -1,4 +1,16 @@
 package com.example.SpringBootSupaBase_StudentManagementSystem;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class StudentModel {
+
+    @Id
+    private final int id;
+
+    private final int roll;
+    private final int marks;
+    private final long sapId;
+
 }
