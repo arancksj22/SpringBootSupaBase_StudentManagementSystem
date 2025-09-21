d00e26414aa5e8f741f8c1ca23ba27f109fc248d
arancksj22 <arancksj@gmail.com>
2025-09-21 20:24:52 +0530

    controller, autowiring + get

--- src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/MainController.java
+++ src/main/java/com/example/SpringBootSupaBase_StudentManagementSystem/MainController.java
@@ -1,10 +1,28 @@
 package com.example.SpringBootSupaBase_StudentManagementSystem;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/StudentSystem")
 public class MainController {
+    MainService service;
 
+    MainController(MainService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<StudentModel> getStudents(){
+        return service.viewAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public StudentModel getStudentById(@PathVariable int id){
+        return service.findById();
+    }
 }
